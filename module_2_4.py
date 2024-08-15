fc81b3083363e203b3cbf929a4ff62196e56505a
Olganord <156664559+Olganord@users.noreply.github.com>
2024-08-15 20:45:52 +0500

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,53 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+
+for number in numbers:
+    if number == 1:
+        continue
+    if all(number % i != 0 for i in range(2, number)):
+        primes.append(number)
+    else:
+        not_primes.append(number)
+print(f"Primes: {primes}")
+print(f"Not Primes: {not_primes}")
+print()
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for number in numbers:
+    if number == 1:
+        continue
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        primes.append(number)
+    else:
+        not_primes.append(number)
+print(f"Primes: {primes}")
+print(f"Not Primes: {not_primes}")
+print()
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for number in numbers:
+    if number == 1:
+        continue
+    if number > 1:
+        for i in range(2, int(number ** 0.5) + 1):
+            if number % i == 0:
+                not_primes.append(number)
+                break
+        else:
+            primes.append(number)
+    else:
+        not_primes.append(number)
+print("primes:", primes)
+print("not_primes:", not_primes)
